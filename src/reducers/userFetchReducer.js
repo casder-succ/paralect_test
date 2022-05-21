72af51e359472cbead7e0bb745d6e49d43c81f5f
casder <casderiopus@gmail.com>
2022-05-21 21:33:31 +0300

    feat: implement reducer processing user fetch

---
+++ src/reducers/userFetchReducer.js
@@ -0,0 +1,34 @@
+import {USER_FETCH_FAIL, USER_FETCH_START, USER_FETCH_SUCCESS} from "../actions/userFetch";
+
+const initialState = {
+    userInfo: {},
+    userRepos: [],
+    loading: false,
+    error: false,
+};
+
+export const userFetchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case USER_FETCH_START:
+            return {
+                ...state,
+                error: false,
+                loading: true,
+            };
+        case USER_FETCH_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                userInfo: action.payload.info,
+                userRepos: action.payload.repos,
+            };
+        case USER_FETCH_FAIL:
+            return {
+                ...state,
+                error: true
+            };
+        default:
+            return state;
+    }
+};
