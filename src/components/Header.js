a552ff5ca492dd47ccaf0986c4f578ecf8861042
casder <casderiopus@gmail.com>
2022-05-21 23:35:20 +0300

    feat: implement and stylize header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import logo from '../logo.png';
+import styled from "styled-components";
+import QueryForm from "../containers/QueryForm";
+
+const HeaderWrapper = styled.header`
+  padding: 16px 41px;
+  height: 72px;
+  width: 100%;
+  
+  background-color: #0064EB;
+  
+  img {
+    width: 40px;
+    height: 40px;
+  }
+`;
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <HeaderWrapper>
+            <div>
+                <a href="https://github.com" target="_blank" rel="noreferrer">
+                    <img src={logo} alt="Github logo" width="40" height="40"/>
+                </a>
+            </div>
+
+            <div>
+                <QueryForm/>
+            </div>
+        </HeaderWrapper>
     );
 };
 
