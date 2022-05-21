47344458660dd07490d0e3a720dec8e86ac5b263
casder <casderiopus@gmail.com>
2022-05-21 23:33:48 +0300

    refactor: add global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import {createGlobalStyle} from "styled-components";
+import Header from "./components/Header";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
+    box-sizing: border-box;
     margin: 0;
+    padding: 0;
+  }
+  
+  body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
@@ -15,6 +21,7 @@ function App() {
     return (
         <>
             <GlobalStyle/>
+            <Header/>
         </>
     );
 }
