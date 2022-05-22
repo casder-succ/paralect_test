51b2cb3748421fdf01612c50e7918c6e9c3300fe
casder <casderiopus@gmail.com>
2022-05-22 15:51:28 +0300

    feat: implement container providing logic to input form

--- src/containers/QueryForm.js
+++ src/containers/QueryForm.js
@@ -1,39 +1,7 @@
-import React from 'react';
 import {connect} from "react-redux";
 import {changeInput} from "../actions/inputForm";
-import {fetchGithubUser} from "../asyncActions/githubUser";
-import styled from "styled-components";
-
-const InputWrapper = styled.input`
-  font-family: Inter, sans-serif;
-  font-size: 14px;
-  
-  height: 40px;
-  width: 500px;
-  padding: 8px 0 8px 44px;
-  
-  border: none;
-  background: #FFFFFF;
-  border-radius: 6px;
-  
-  &:focus {
-    outline: none;
-    border: none;
-  }
-`;
-
-const QueryForm = ({query, onChange, onKeyDown}) => {
-    return (
-        <>
-            <InputWrapper
-                value={query}
-                onChange={(event) => onChange(event.target.value)}
-                onKeyDown={(event) => onKeyDown(event.key, query)}
-                type="text"
-            />
-        </>
-    );
-};
+import {fetchGithubUser, fetchUserRepos} from "../asyncActions/githubUser";
+import Input from "../components/Input";
 
 const mapStateToProps = (state) => ({
     query: state.inputForm.query,
@@ -46,8 +14,9 @@ const mapDispatchToProps = (dispatch) => ({
     onKeyDown: (key, user) => {
         if (key === 'Enter') {
             dispatch(fetchGithubUser(user));
+            dispatch(fetchUserRepos(user, 1));
         }
     }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(QueryForm);
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
