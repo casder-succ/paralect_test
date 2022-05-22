45e056987d3e309f602901cf4b1ab9c5dddcdb20
casder <casderiopus@gmail.com>
2022-05-22 17:12:26 +0300

    feat: add message showing basing on status

--- src/components/UserWrapper.js
+++ src/components/UserWrapper.js
@@ -2,41 +2,47 @@ import React from 'react';
 import UserInfo from "./UserInfo";
 import UserRepos from "./UserRepos";
 import styled from "styled-components";
+import Message from "./Message";
+import {messageTypes} from "../util/messageTypes";
 
 const ProfileContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   column-gap: 96px;
-  
+
   max-width: 92%;
   margin: 28px auto 0;
 `;
 
 const UserWrapper = ({loading, error, userInfo, userRepos, handlePageChange}) => {
     if (loading) {
-        return <h1>Loading...</h1>
+        return (
+            <Message type={messageTypes.LOADING}/>
+        )
     }
 
     if (error) {
-        return <h1>Error...</h1>
+        return (
+            <Message type={messageTypes.USER_NOT_FOUND}/>
+        )
     }
 
     if (!userInfo?.login) {
-        return <h1>Start...</h1>
+        return (
+            <Message type={messageTypes.START_SEARCH}/>
+        )
     }
 
     return (
-        <main>
-            <ProfileContainer>
-                <UserInfo userInfo={userInfo} />
-                <UserRepos
-                    userRepos={userRepos}
-                    quantity={userInfo.public_repos}
-                    onPageChange={handlePageChange}
-                />
-            </ProfileContainer>
-        </main>
+        <ProfileContainer>
+            <UserInfo userInfo={userInfo}/>
+            <UserRepos
+                userRepos={userRepos}
+                quantity={userInfo.public_repos}
+                onPageChange={handlePageChange}
+            />
+        </ProfileContainer>
     );
 };
 
