43f81e93dedac9dc7d3f9444d85772cc7347b67b
casder <casderiopus@gmail.com>
2022-05-22 15:48:55 +0300

    feat: implement repository component

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import styled from "styled-components";
 
-const Repository = () => {
+const RepoWrapper = styled.article`
+  padding: 24px 32px;
+  background: #FFFFFF;
+  border-radius: 6px;
+
+  &:not(:last-child) {
+    margin-bottom: 24px;
+  }
+  
+  .title {
+    margin-bottom: 16px;
+
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 29px;
+  }
+  
+  .content {
+    font-size: 16px;
+    line-height: 19px;
+  }
+`;
+
+const Repository = ({title, url, content}) => {
     return (
-        <div>
-            <h3>
-                <a href=""></a>
+        <RepoWrapper>
+            <h3 className="title">
+                <a href={url}>{title}</a>
             </h3>
-            <p></p>
-        </div>
+            <p className="content">{content}</p>
+        </RepoWrapper>
     );
 };
 
