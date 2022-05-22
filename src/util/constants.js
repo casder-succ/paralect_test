dc882d88dad1952967b256db14df0ead58beaf65
casder <casderiopus@gmail.com>
2022-05-22 15:52:00 +0300

    feat: add possibility to fetch repos pages

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,4 +1,4 @@
 export const constants = {
     userUrl: (user) => `https://api.github.com/users/${user}`,
-    reposUrl: (user) => `https://api.github.com/users/${user}/repos`,
+    reposUrl: (user, page) => `https://api.github.com/users/${user}/repos?per_page=4&page=${page}`,
 };
