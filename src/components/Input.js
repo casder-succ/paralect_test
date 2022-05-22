4e8b6f210eab1326bf48d399790f696915fc8e01
casder <casderiopus@gmail.com>
2022-05-22 15:50:17 +0300

    feat: separate layout from logic

---
+++ src/components/Input.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from "styled-components";
+import searchIcon from "./icon-image.svg";
+
+const InputWrapper = styled.input`
+  position: relative;
+  
+  height: 40px;
+  width: 500px;
+  padding: 8px 0 8px 44px;
+  
+  border: none;
+  background-color: #FFFFFF;
+  border-radius: 6px;
+
+  font-family: Inter, sans-serif;
+  font-size: 14px;
+  background-image: url(${searchIcon});
+  background-repeat: no-repeat;
+  background-position:19px 13px ;
+  
+  &:focus {
+    outline: none;
+    border: none;
+  }
+`;
+
+const Input = ({query, onChange, onKeyDown}) => {
+    return (
+        <>
+            <InputWrapper
+                value={query}
+                onChange={(event) => onChange(event.target.value)}
+                onKeyDown={(event) => onKeyDown(event.key, query)}
+                type="text"
+            />
+        </>
+    );
+};
+
+export default Input;
