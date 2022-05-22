24c354e6d13aa0a76053c8cde9dfab618e2b30e7
casder <casderiopus@gmail.com>
2022-05-22 15:52:17 +0300

    feat: implement container providing logic to user profile

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,10 +1,18 @@
 import {connect} from "react-redux";
 import UserWrapper from "../components/UserWrapper";
+import {fetchUserRepos} from "../asyncActions/githubUser";
 
 const mapStateToProps = (state) => ({
     userInfo: state.userFetch.userInfo,
+    userRepos: state.userRepos.userRepos,
     loading: state.userFetch.loading,
     error: state.userFetch.error,
 });
 
-export default connect(mapStateToProps)(UserWrapper);
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    handlePageChange: (page) => {
+        dispatch(fetchUserRepos(ownProps.userInfo?.login, page));
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserWrapper);
